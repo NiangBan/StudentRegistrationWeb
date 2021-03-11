b71cf3cffdb34efd2220e574ee614729418cf34e
ChitMyoKo <chitmyoko.2001@gmail.com>
2021-03-11 10:02:56 +0630

    student register

--- StudentRegistrationWeb/Controllers/StudentController.cs
+++ StudentRegistrationWeb/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentRegistrationWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,18 @@ namespace StudentRegistrationWeb.Controllers
         // GET: Student
         public ActionResult StudentList()
         {
+            return View();
+        }
+        public ActionResult StudentRegister()
+        {
+            TestModel t = new TestModel();
+            t.Name = "CU";
+            TestModel t1 = new TestModel();
+            t1.Name = "TU";
+            List<TestModel> l = new List<TestModel>();
+            l.Add(t);
+            l.Add(t1);
+            ViewBag.UniversityList = l;
             return View();
         }
     }
